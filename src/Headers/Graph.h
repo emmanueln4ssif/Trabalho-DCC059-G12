1af6ff6f96616d31e6de5571188bb5ad100c3f9c
emmanueln4ssif <emmanuel.nassif@gmail.com>
2023-06-07 00:37:39 -0300

    Adicionando funcionalidades para a classe Graph

--- src/Headers/Graph.h
+++ src/Headers/Graph.h
@@ -6,6 +6,33 @@ using namespace std;
 
 class Graph{
 
+    private:
+        int ordem;
+        bool ehDirecionado;
+        bool ehPonderadoArestas;
+        bool ehPonderadoNos;
+        int numArestas;
+        int posicao;
+
+        Node *primeiro;
+        Node *ultimo;
+
+    public:
+        Graph(int ordem, bool ehDirecionado, bool ehPonderadoArestas, bool ehPonderadoNos);
+        ~Graph();
+
+        int getOrdem();
+        bool getEhDirecionado();
+        bool getEhPonderadoArestas();
+        bool getEhPonderadoNos();
+        Node* getPrimeiro();
+        Node* getUltimo();
+        Node* getNodePosicao();
+
+        void insereNo(int id);
+        void insereAresta(int id, int idNo, int peso);
+        void removeNo(int id);
+        void pesquisaNo(int id);
 
 };
 
--- src/Sources/Graph.cpp
+++ src/Sources/Graph.cpp
@@ -1,1 +1,67 @@
-#include "Graph.h"
+#include "src/Headers/Graph.h"
+
+//** Construtor do Grafo**
+Graph::Graph(int ordem, bool ehDirecionado, bool ehPonderadoArestas, bool ehPonderadoNos)
+{
+    this->ordem = ordem;
+    this->ehDirecionado = ehDirecionado;
+    this->ehPonderadoArestas = ehPonderadoArestas;
+    this->ehPonderadoNos = ehPonderadoNos;
+    this->numArestas = 0;
+    this->primeiro = this->ultimo = nullptr;
+}
+
+//**Getters necessários**
+int Graph::getOrdem()
+{
+    return this->ordem;
+}
+
+bool Graph::getEhDirecionado()
+{
+    return this->ehDirecionado;
+}
+
+bool Graph::getEhPonderadoArestas()
+{
+    return this->ehPonderadoArestas;
+}
+
+bool Graph::getEhPonderadoNos()
+{
+    return this->ehPonderadoNos;
+}
+
+Node *Graph::getPrimeiro()
+{
+    return this->primeiro;
+}
+Node *Graph::getUltimo()
+{
+    return this->ultimo;
+}
+
+// Node* Graph::getNodePosicao(int posicao){
+// return this->posicao;
+//}
+
+//A) INSERÇÃO DE NÓS
+
+void Graph::insereNo(int id){
+
+    //verificamos se já existe algum nó. Se houver, cria-se um novo Nó que aponta o último para o novo, e o novo passa a ser o último
+    if(primeiro != nullptr){
+        Node *newNode = new Node(id);
+        newNode->setPosicao(this->posicao);
+        ultimo->setProx(newNode);
+        ultimo = newNode;
+    } else{
+        Node *newNode = new Node(id);
+        newNode->setPosição(this->posicao);
+        primeiro = newNode;
+        ultimo = newNode;
+    }
+
+    this->posicao = this->posicao + 1;
+
+}
